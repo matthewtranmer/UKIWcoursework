25abbf837eefb86409d6cbf1403840228feb58ca
matthewtranmer <matthew.tranmer@outlook.com>
2022-04-29 11:47:37 -0100

    Update

--- static/js/home.js
+++ static/js/home.js
@@ -8,14 +8,14 @@ function splash(e){
     
     
     if(width > 1400){
-        
         document.getElementById("textbox").setAttribute("style", "display: inline-block; width: 45%;")
         document.getElementById("map").setAttribute("style", "display: inline-block; width: 47%; float: right; margin-right: 30px;")
+        document.getElementById("canalimage").setAttribute("style", "float: right; display: inline; width: 45%; padding-right: 31px;")
     }
     else{
-        
         document.getElementById("textbox").setAttribute("style", "display: block; width: 85%; margin: auto; margin-bottom: 20px")
         document.getElementById("map").setAttribute("style", "display: block; width: 87%; float: none; margin: auto;")
+        document.getElementById("canalimage").setAttribute("style", "float: none; display: block; width: 85%; padding-right: 0px;")
     }
 
     if (width > 1000){
